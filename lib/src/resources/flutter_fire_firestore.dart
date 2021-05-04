bdeb4563cfe22ca669776fd1d99d11e1e4a05a57
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-04 22:28:43 +0530

    Firebase boilerplate

---
+++ lib/src/resources/flutter_fire_firestore.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<bool> addUsernameAndProfile(String username, String profilePicPath) {
+  //save username and profile pic path to current user id
+}
---
+++ lib/src/resources/flutter_fire_storage.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+//import firebase storage
+import 'dart:io';
+
+Future<String> saveProfilePic(File compressedImage) {
+  //save compressed profile picture and return path to image.
+}
