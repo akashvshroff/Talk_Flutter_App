9a7ebaa7f22a557d50ce8495f1146b2beec1d827
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-04 12:56:22 +0530

    Initial navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'screens/login.dart';
+import 'screens/sign_up.dart';
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Fira',
+      ),
+      title: 'talk',
+      home: LoginPage(),
+      onGenerateRoute: routes,
+    );
+  }
+
+  Route routes(RouteSettings settings) {
+    String routeName = settings.name;
+    if (routeName == '/') {
+      //loading page
+    } else if (routeName == '/signup') {
+      //signup page
+      return MaterialPageRoute(builder: (context) {
+        return SignUpPage();
+      });
+    } else if (routeName == '/login') {
+      //login page
+      return MaterialPageRoute(builder: (context) {
+        return LoginPage();
+      });
+    } else if (routeName == '/conversations') {
+      //conversation home page
+    }
   }
 }
