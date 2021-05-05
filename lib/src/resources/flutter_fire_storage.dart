cb2dafffc125e6394ca0898d9047f24033f7c460
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-05 11:41:09 +0530

    Firebase Storage profile pic save

--- lib/src/resources/flutter_fire_storage.dart
+++ lib/src/resources/flutter_fire_storage.dart
@@ -1,7 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
-//import firebase storage
 import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
 
-Future<String> saveProfilePic(File compressedImage) {
+Future<String> saveProfilePic(File image) async {
   //save compressed profile picture and return path to image.
+  String filePath = 'images/${image.path}_${DateTime.now()}.png';
+  await FirebaseStorage.instance.ref().child(filePath).putFile(image);
+  return await FirebaseStorage.instance.ref().child(filePath).getDownloadURL();
 }
