5c97fb609c0786c8ab0e9e324ec4ea2d91df3547
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-05 12:52:51 +0530

    Firebase firestore onsignup

--- lib/src/resources/flutter_fire_firestore.dart
+++ lib/src/resources/flutter_fire_firestore.dart
@@ -1,7 +1,35 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-Future<bool> addUsernameAndProfile(String username, String profilePicPath) {
+Future<bool> addUsernameAndProfileOnSignUp(
+    String username, String profilePicPath) async {
   //save username and profile pic path to current user id
+  String defaultPath =
+      'https://firebasestorage.googleapis.com/v0/b/talk-chat-app-66f32.appspot.com/o/default_profile.png?alt=media&token=b3d50e26-7355-4a8f-8340-750cee75ed81';
+  try {
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('Users').doc(uid);
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot snapshot = await transaction.get(documentReference);
+      if (!snapshot.exists) {
+        documentReference.set({
+          'username': username,
+          'profile_pic_path':
+              profilePicPath != '' ? profilePicPath : defaultPath,
+          'connections': [],
+          'active_conversations': []
+        });
+      }
+    });
+    return true;
+  } catch (e) {
+    Fluttertoast.showToast(msg: e.toString());
+    return false;
+  }
+}
+
+void toast(message) {
+  Fluttertoast.showToast(msg: message, toastLength: Toast.LENGTH_LONG);
 }
