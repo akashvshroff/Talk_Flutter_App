93f387b268075dc344ea4993a0d9813c85270edf
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-06 13:25:31 +0530

    Active conversation stream

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,1 +1,35 @@
-class Bloc {}
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../models/active_conversation_model.dart';
+
+class Bloc {
+  //StreamControllers
+  final _activeConversations = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .snapshots();
+
+  //getters
+  Stream<List<ActiveConversationModel>> get activeConversationStream =>
+      _activeConversations.transform(activeConversationsTransformer());
+
+  activeConversationsTransformer() {
+    return StreamTransformer<DocumentSnapshot,
+            List<ActiveConversationModel>>.fromHandlers(
+        handleData: (DocumentSnapshot snapshot, sink) {
+      var activeConversations = snapshot.data()['active_conversations'];
+      List<ActiveConversationModel> activeConversationModels = [];
+      activeConversations.forEach((element) {
+        activeConversationModels.add(ActiveConversationModel.fromMap(element));
+      });
+      sink.add(activeConversationModels);
+    });
+  }
+
+  void dispose() {}
+}
