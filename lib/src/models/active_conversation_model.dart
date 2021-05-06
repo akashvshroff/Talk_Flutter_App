d5500ba0c447a68face69d888c888cd395f07b98
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-06 13:25:23 +0530

    Active conversations listview

--- lib/src/models/active_conversation_model.dart
+++ lib/src/models/active_conversation_model.dart
@@ -9,4 +9,20 @@ class ActiveConversationModel {
       this.profilePicPath,
       this.conversationId,
       this.newMessage});
+
+  ActiveConversationModel.fromMap(Map<String, dynamic> data) {
+    this.username = data['username'];
+    this.profilePicPath = data['profile_pic_path'];
+    this.conversationId = data['conversation_id'];
+    this.newMessage = data['new_message'];
+  }
+
+  toMap() {
+    return {
+      'conversation_id': this.conversationId,
+      'new_message': this.newMessage,
+      'profile_pic_path': this.profilePicPath,
+      'username': this.username,
+    };
+  }
 }
