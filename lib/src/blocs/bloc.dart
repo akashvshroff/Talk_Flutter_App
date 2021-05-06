4f31863e3150d5b4993606d945eba3d769529139
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-06 13:59:57 +0530

    Connection and profile stream and transformers

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -6,17 +6,31 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/active_conversation_model.dart';
+import '../models/connection_model.dart';
+import '../models/user_profile_model.dart';
 
 class Bloc {
   //StreamControllers
-  final _activeConversations = FirebaseFirestore.instance
+  final _activeConversation = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .snapshots();
+
+  final _connections = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .snapshots();
+
+  final _profile = FirebaseFirestore.instance
       .collection('Users')
       .doc(FirebaseAuth.instance.currentUser.uid)
       .snapshots();
 
   //getters
   Stream<List<ActiveConversationModel>> get activeConversationStream =>
-      _activeConversations.transform(activeConversationsTransformer());
+      _activeConversation.transform(activeConversationsTransformer());
+  Stream<List<ConnectionModel>> get connectionsStream =>
+      _connections.transform(connectionsTransformer());
 
   activeConversationsTransformer() {
     return StreamTransformer<DocumentSnapshot,
@@ -28,6 +42,29 @@ class Bloc {
         activeConversationModels.add(ActiveConversationModel.fromMap(element));
       });
       sink.add(activeConversationModels);
+    });
+  }
+
+  connectionsTransformer() {
+    return StreamTransformer<DocumentSnapshot,
+            List<ConnectionModel>>.fromHandlers(
+        handleData: (DocumentSnapshot snapshot, sink) {
+      var connections = snapshot.data()['connections'];
+      List<ConnectionModel> connectionModels = [];
+      connections.forEach((element) {
+        connectionModels.add(ConnectionModel.fromMap(element));
+      });
+      sink.add(connectionModels);
+    });
+  }
+
+  profileTransformer() {
+    return StreamTransformer<DocumentSnapshot, UserProfileModel>.fromHandlers(
+        handleData: (DocumentSnapshot snapshot, sink) {
+      Map data = snapshot.data();
+      sink.add(UserProfileModel(
+          username: data['username'],
+          profilePicPath: data['profile_pic_path']));
     });
   }
 
