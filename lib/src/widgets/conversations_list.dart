818a5e4fe8b23b07bca599183774c5a88139dd90
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-06 13:26:04 +0530

    conversation listview

--- lib/src/widgets/conversations_list.dart
+++ lib/src/widgets/conversations_list.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../blocs/provider.dart';
+import '../resources/flutter_fire_firestore.dart';
+
+import '../models/active_conversation_model.dart';
+
 class ConversationsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Bloc bloc = Provider.of(context);
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
@@ -17,7 +23,12 @@ class ConversationsList extends StatelessWidget {
                   )),
               addNewButton(),
             ],
-          )
+          ),
+          SizedBox(
+            height: 6.0,
+          ),
+          Divider(),
+          Expanded(child: activeConversationsList(bloc)),
         ],
       ),
     );
@@ -53,5 +64,56 @@ class ConversationsList extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget activeConversationsList(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.activeConversationStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasData == false) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return ListView.separated(
+            separatorBuilder: (context, index) {
+              return Divider(
+                height: 2.0,
+              );
+            },
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              ActiveConversationModel activeConversation = snapshot.data[index];
+              return Container(
+                margin: EdgeInsets.only(top: 4.0, bottom: 4.0),
+                child: ListTile(
+                  title: Text(' ${activeConversation.username}',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      )),
+                  leading: CircleAvatar(
+                    child: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(activeConversation.profilePicPath),
+                    ),
+                  ),
+                  trailing: activeConversation.newMessage
+                      ? Icon(
+                          Icons.touch_app,
+                          color: Colors.green,
+                          size: 30,
+                        )
+                      : Container(height: 0.0, width: 0.0),
+                  onTap: () => openConversation(activeConversation),
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+
+  void openConversation(ActiveConversationModel activeConversation) async {
+    bool result = await markConversationAsRead(activeConversation);
+    print(result);
   }
 }
