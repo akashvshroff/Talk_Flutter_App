127dcf9276e5a9b7fc488f33f969e99f416319a0
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-06 13:59:42 +0530

    Added frommap

--- lib/src/models/connection_model.dart
+++ lib/src/models/connection_model.dart
@@ -4,4 +4,10 @@ class ConnectionModel {
   String profilePicPath;
 
   ConnectionModel({this.userId, this.username, this.profilePicPath});
+
+  ConnectionModel.fromMap(Map data) {
+    this.userId = data['user_id'];
+    this.username = data['username'];
+    this.profilePicPath = data['profile_pic_path'];
+  }
 }
