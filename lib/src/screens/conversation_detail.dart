fef5c55deb1487b4497bbcf121d440571a6507dd
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-08 00:09:49 +0530

    Boilerplate

---
+++ lib/src/screens/conversation_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../blocs/provider.dart';
+
+class ConversationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
