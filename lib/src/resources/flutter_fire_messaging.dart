1141be43563a60e077876d98e8269e3e9b83a307
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-10 13:50:43 +0530

    Got details from notification

--- lib/src/resources/flutter_fire_messaging.dart
+++ lib/src/resources/flutter_fire_messaging.dart
@@ -2,9 +2,10 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'local_notification_service.dart';
 
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  var data = message.data;
-  String title = data['title'];
-  String messageText = data['messageText'];
+  print('handling background message: ${message.messageId}');
+  var notification = message.notification;
+  String title = notification.title;
+  String messageText = notification.body;
   String conversationId = '';
   NotificationService()
       .showNotification(message.messageId, title, messageText, conversationId);
