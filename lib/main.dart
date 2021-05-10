720176687d2845256de325933d62e82b91aecb29
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-05-10 13:50:21 +0530

    Added notifs dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'src/resources/local_notification_service.dart';
+import 'src/resources/flutter_fire_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService().init();
   await Firebase.initializeApp();
+  await FirebaseMessaging.instance.getToken();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   runApp(App());
 }
